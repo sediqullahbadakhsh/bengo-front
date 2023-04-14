0534b5fe91c3ced8b16512585bce44da526825ff
sediqbadakhsh <sediq@invasso.com>
2023-04-14 06:48:14 +0430

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import AnalyticsDashboard from "./components/AnalyticsDashboard";
 import "./App.css";
 import Search from "./components/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="">
-      <Search />
-      {/* <AnalyticsDashboard /> */}
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path="/analytics" element={<AnalyticsDashboard />} />
+      </Routes>
     </div>
   );
 }
