0e341ac53c779b9dcf3609763cf6945a7b3d86ba
sediqbadakhsh <sediq@invasso.com>
2023-04-14 21:02:03 +0430

    Updates

--- src/tools/fetchIpAddress.js
+++
@@ -1,8 +0,0 @@
-/* Get users IP address */
-async function fetchIpAddress() {
-  const response = await fetch("https://api.ipify.org?format=json");
-  const data = await response.json();
-  return data.ip;
-}
-
-export default fetchIpAddress;
